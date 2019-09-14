aabc2afb5cd2a3bb6d638063ada5767686fb8f1a
oschmidt <mondoshivan@me.com>
2019-09-14 14:08:46 +0200

    [add] unit test check

---
+++ server/public/js/test/fizzBuzz.test.ts
@@ -0,0 +1,7 @@
+/// <reference types="jest" />
+
+import {fizzBuzz} from "../app/fizzBuzz";
+test("FizzBuzz test", () =>{
+    expect(fizzBuzz(2)).toBe("1 2 ");
+    expect(fizzBuzz(3)).toBe("1 2 Fizz ");
+});
